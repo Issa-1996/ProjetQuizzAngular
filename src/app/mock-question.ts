635380c72950d5b3c70357d90b2f89d1fa38e97c
Issa-1996 <issasarr444@gmail.com>
2021-07-03 01:33:06 +0000

    modifier question juste

--- src/app/mock-question.ts
+++ src/app/mock-question.ts
@@ -3,28 +3,28 @@ import { Question, Reponse } from './question';
 export const QUESTIONS: Question[] = [
     {
         id: 1,
-        titre: 'Dans laquelle de ces propositions l’invention n\'est-elle pas bien attribuée à son inventeur ?',
+        titre: ' Qui a inventé la machine à vapeur ?',
         type: 'radio',
         point: 20,
         reponse: [
             {
                 id: 1,
-                titre: 'La machine à vapeur de James Watt',
-                valeur: true
+                titre: 'Isaac Newton',
+                valeur: false
             },
             {
                 id: 2,
-                titre: 'Le Téléphone d\'Alexander Bell',
+                titre: 'Galileo Galilei.',
                 valeur: false
             },
             {
                 id: 3,
-                titre: 'Le phonographe de Thomas Edison',
-                valeur: false
+                titre: 'James Watt.',
+                valeur: true
             },
             {
                 id: 4,
-                titre: 'La montre de Leon Foucault',
+                titre: 'Christophe Colomb',
                 valeur: false
             }
         ]
@@ -66,12 +66,12 @@ export const QUESTIONS: Question[] = [
             {
                 id: 1,
                 titre: 'Courant éléctrique.',
-                valeur: true
+                valeur: false
             },
             {
                 id: 2,
                 titre: 'Tension éléctrique.',
-                valeur: false
+                valeur: true
             },
             {
                 id: 3,
@@ -93,8 +93,8 @@ export const QUESTIONS: Question[] = [
         reponse: [
             {
                 id: 1,
-                titre: 'Les batraciens.',
-                valeur: true
+                titre: 'les poissons.',
+                valeur: false
             },
             {
                 id: 2,
@@ -104,7 +104,7 @@ export const QUESTIONS: Question[] = [
             {
                 id: 3,
                 titre: 'Les crustacés.',
-                valeur: false
+                valeur: true
             },
             {
                 id: 4,
@@ -115,28 +115,28 @@ export const QUESTIONS: Question[] = [
     },
     {
         id: 5,
-        titre: 'Le BCG est un vaccin contre :',
+        titre: 'Quel est la forma factorisé de: (a-b)(a+b)?',
         type: 'radio',
         point: 30,
         reponse: [
             {
                 id: 1,
-                titre: 'Le tétanos.',
-                valeur: true
-            },
-            {
-                id: 2,
-                titre: 'La grippe.',
+                titre: '(a2 +b2)2.',
                 valeur: false
             },
             {
+                id: 2,
+                titre: '(a-b)2.',
+                valeur: true
+            },
+            {
                 id: 3,
-                titre: 'La rage.',
+                titre: '(a-b)2.',
                 valeur: false
             },
             {
                 id: 4,
-                titre: 'La tuberculose',
+                titre: 'a2 +2ab - b2.',
                 valeur: false
             }
         ]
@@ -166,11 +166,6 @@ export const QUESTIONS: Question[] = [
                 id: 4,
                 titre: 'O-',
                 valeur: true
-            },
-            {
-                id: 5,
-                titre: 'AB+',
-                valeur: false
             }
         ]
     },
@@ -183,7 +178,7 @@ export const QUESTIONS: Question[] = [
             {
                 id: 1,
                 titre: 'Watt.',
-                valeur: true
+                valeur: false
             },
             {
                 id: 2,
@@ -193,7 +188,7 @@ export const QUESTIONS: Question[] = [
             {
                 id: 3,
                 titre: 'Joule.',
-                valeur: false
+                valeur: true
             },
             {
                 id: 4,
@@ -239,7 +234,7 @@ export const QUESTIONS: Question[] = [
             {
                 id: 1,
                 titre: 'Newton.',
-                valeur: true
+                valeur: false
             },
             {
                 id: 2,
@@ -249,7 +244,7 @@ export const QUESTIONS: Question[] = [
             {
                 id: 3,
                 titre: 'Lavoisier.',
-                valeur: false
+                valeur: true
             },
             {
                 id: 4,
@@ -260,29 +255,29 @@ export const QUESTIONS: Question[] = [
     },
     {
         id: 10,
-        titre: 'Comment appelle-t-on l\'énergie liée au mouvement ?',
+        titre: 'Comment appelle-t-on un triangle Aiguille  qui a quatre cotés ?',
         type: 'checkbox',
         point: 30,
         reponse: [
             {
                 id: 1,
-                titre: 'Energie chimique.',
-                valeur: true
+                titre: 'Triangle équivoque.',
+                valeur: false
             },
             {
                 id: 2,
-                titre: 'Energie thermique.',
+                titre: 'Triangle isocèle.',
                 valeur: false
             },
             {
                 id: 3,
-                titre: 'Energie solaire.',
+                titre: 'Triangle équilatéral.',
                 valeur: false
             },
             {
                 id: 4,
-                titre: 'Energie cinétique',
-                valeur: false
+                titre: 'Aucune des reponses',
+                valeur: true
             }
         ]
     }
